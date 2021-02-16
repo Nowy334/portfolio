3156c37c283e4a3adae443095d8e6c6a6ceac1a8
Nowy334 <krzysnowaks@gmail.com>
2021-02-16 15:43:42 +0100

    update

--- js/index.js
+++ js/index.js
@@ -1,12 +1,18 @@
+gsap.registerPlugin(ScrollTrigger);
 //navigation
 const nav = document.querySelector(".navigation");
 const header = document.querySelector("#header");
+const sectionAbout = document.querySelector("#about");
+const sectionSkills = document.querySelector("#skills");
+const portfolio = document.querySelector("#portfolio");
 const links = document.querySelectorAll(".navigation__link");
 
+
 window.addEventListener("scroll", () => {
-  let top = header.offsetHeight;
+  let top = header.offsetHeight - 100;
+  let fromTop = window.scrollY;
   
-  if(window.scrollY >= top){
+  if(fromTop >= top){
     links.forEach(elem => elem.classList.add("sticky-text"));
     nav.classList.add("sticky");
   }else {
@@ -14,6 +20,40 @@ window.addEventListener("scroll", () => {
     nav.classList.remove("sticky");
   }
 })
+
+const items = document.querySelectorAll(".navigation__list a");
+ 
+for (const item of items) {
+  item.addEventListener("click", clickHandler);
+}
+ 
+function clickHandler(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+  const offsetTop = document.querySelector(href).offsetTop;
+  const setTop = offsetTop-120;
+
+  if(href === "#skills"){
+    let setTopSkills = offsetTop+120;
+    scroll({
+      top: setTopSkills,
+      behavior: "smooth"
+    });
+  }else if(href === "#portfolio"){
+    let setTopPortfolio = offsetTop-60;
+
+    scroll({
+      top: setTopPortfolio,
+      behavior: "smooth"
+    });
+
+  }else {
+    scroll({
+      top: setTop,
+      behavior: "smooth"
+    });
+  }
+}
 
 
 //animation page
@@ -35,5 +75,32 @@ const tl2 = new TimelineMax({delay:3, repeat: -1 })
   .to(all, 3, {y: -50})
   .to(all, 3, {y: 0})
 
+  //section about
+
+  let about = gsap.timeline({
+    scrollTrigger: {
+      trigger: '.about',
+      start: "center bottom"
+    }
+  });
+  about.from(".about__container-image", {scale: 0.4, opacity:0, duration: 2})
+       .from(".about__container-icons--box", {y:100, opacity:0, duration:2}, 0)
+       .from(".about__container-title", {x:200, opacity:0, duration:2}, 0)
+       .from(".about__container-text", {x:200, opacity:0, duration:2}, 0)
+       .from(".about__container-image--border", {rotation:360, opacity:0, duration:2}, 0)
+       .from(".headline-1", {y:-200, opacity:0, duration:2}, 0);
+
+  
+let skills = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.skills',
+    start: "center bottom"
+  }
+});
+skills.from(".circle-container-element", {transform:"translate3d(0, 0, 0)", duration: 2})
+
+
+
+       
 
 
