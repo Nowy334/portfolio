56979696f6ad0eb236b810281b4728a93fb8ed12
Nowy334 <krzysnowaks@gmail.com>
2021-03-13 16:07:23 +0100

    add media queries

--- js/index.js
+++ js/index.js
@@ -6,19 +6,18 @@ const sectionAbout = document.querySelector("#about");
 const sectionSkills = document.querySelector("#skills");
 const portfolio = document.querySelector("#portfolio");
 const links = document.querySelectorAll(".navigation__link");
-
+const btnHeader = document.querySelector(".btn");
 
 window.addEventListener("scroll", () => {
-  let top = header.offsetHeight - 100;
   let fromTop = window.scrollY;
-  
-  if(fromTop >= top){
-    links.forEach(elem => elem.classList.add("sticky-text"));
-    nav.classList.add("sticky");
-  }else {
-    links.forEach(elem => elem.classList.remove("sticky-text"));
-    nav.classList.remove("sticky");
-  }
+  let top = header.offsetHeight - 100;
+    if(fromTop >= top){
+      links.forEach(elem => elem.classList.add("sticky-text"));
+      nav.classList.add("sticky");
+    }else {
+      links.forEach(elem => elem.classList.remove("sticky-text"));
+      nav.classList.remove("sticky");
+    }
 })
 
 const items = document.querySelectorAll(".navigation__list a");
@@ -26,7 +25,8 @@ const items = document.querySelectorAll(".navigation__list a");
 for (const item of items) {
   item.addEventListener("click", clickHandler);
 }
- 
+
+
 function clickHandler(e) {
   e.preventDefault();
   const href = this.getAttribute("href");
@@ -34,27 +34,74 @@ function clickHandler(e) {
   const setTop = offsetTop-120;
 
   if(href === "#skills"){
-    let setTopSkills = offsetTop+120;
+    let setTopSkills;
+    if(window.screen.width < 600 || window.screen.height < 800 ) {
+      setTopSkills = offsetTop-40;
+    }else {
+      setTopSkills = offsetTop+120;
+    }
     scroll({
       top: setTopSkills,
       behavior: "smooth"
     });
   }else if(href === "#portfolio"){
-    let setTopPortfolio = offsetTop-60;
+    let setTopPortfolio = offsetTop-20;
 
     scroll({
       top: setTopPortfolio,
       behavior: "smooth"
     });
 
-  }else {
+  }else if(href === "#about"){
+    let setTopPortfolio = offsetTop-90;
+
+    scroll({
+      top: setTopPortfolio,
+      behavior: "smooth"
+    });
+  
+  }else if(href === "#contact"){
+    let setTopPortfolio = offsetTop-40;
+    scroll({
+      top: setTopPortfolio,
+      behavior: "smooth"
+    });
+  
+   }else {
     scroll({
       top: setTop,
       behavior: "smooth"
     });
   }
 }
+//btn header
+btnHeader.addEventListener("click", function(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+  const offsetTop = document.querySelector(href).offsetTop;
+  scroll({
+    top: offsetTop-20,
+    behavior: "smooth"
+  });
+})
 
+//mobile menu 
+const menuBtn = document.querySelector('.menu-btn');
+const navig = document.querySelector(".navigation__list");
+
+menuBtn.addEventListener('click', ()=>{
+
+  menuBtn.classList.toggle('open');
+  navig.classList.toggle('nav-active');
+
+});
+
+for (let i = 0; i < links.length; i++) {
+  links[i].addEventListener('click', () => {
+      menuBtn.classList.toggle('open');
+      navig.classList.toggle('nav-active');
+  });
+}
 
 //animation page
 
@@ -84,11 +131,11 @@ const tl2 = new TimelineMax({delay:3, repeat: -1 })
     }
   });
   about.from(".about__container-image", {scale: 0.4, opacity:0, duration: 2})
-       .from(".about__container-icons--box", {y:100, opacity:0, duration:2}, 0)
+       .from(".about .about__container-icons--box", {y:100, opacity:0, duration:2}, 0)
        .from(".about__container-title", {x:200, opacity:0, duration:2}, 0)
        .from(".about__container-text", {x:200, opacity:0, duration:2}, 0)
        .from(".about__container-image--border", {rotation:360, opacity:0, duration:2}, 0)
-       .from(".headline-1", {y:-200, opacity:0, duration:2}, 0);
+       .from(".headline-1", {y:-50, opacity:0, duration:2}, 0);
 
   
 let skills = gsap.timeline({
@@ -97,7 +144,42 @@ let skills = gsap.timeline({
     start: "center bottom"
   }
 });
-skills.from(".circle-container-element", {transform:"translate3d(0, 0, 0)", duration: 2})
+
+skills.from(".circle-container-element", {transform:"translate3d(0, 0, 0)", duration: 2});
+
+let projects = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.projects',
+    start: "center bottom"
+  }
+});
+
+projects.from('.headline-2',{y:-35, opacity:0, duration:2})
+        .from('.projects__item-1', {x:-35, opacity:0, duration:2}, 0)
+        .from('.projects__item-2', {x:35, opacity:0, duration:2}, 0)
+        .from('.projects__item-3', {x:-35, opacity:0, duration:2}, 0)
+        .from('.projects__item-4', {x:35, opacity:0, duration:2}, 0);
+
+
+let contact = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.contact',
+    start: "center bottom"
+  }
+});
+
+contact.from('.headline-3',{y:-50, opacity:0, duration:2})
+       .from(".contact__mail", {scale: 0.4, opacity:0, duration: 2}, 0)
+
+let footer = gsap.timeline({
+  scrollTrigger: {
+    trigger: '.footer',
+    start: "center bottom"
+  }
+});
+
+footer.from(".footer .about__container-icons--box", {y:50, opacity:0, duration:2})
+
 
 
 
